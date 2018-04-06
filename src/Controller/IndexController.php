066bb572199e1af369fa5accdcb24608f2940c20
DreDasLT <dre2kas@gmail.com>
2018-04-06 22:43:21 +0300

    Styled question/comment block. Added footer. Made sidebar menus to get router and make menu class active.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,11 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
 	/**
-	 * @Route("/", name="index")
+	 * @Route("/Pagrindinis", name="Pagrindinis")
 	 * @return Response
 	 */
 	public function index(): Response
 	{
 		return $this->render('index.html.twig');
 	}
+
+    /**
+     * @Route("/questions", name="questions")
+     * @return Response
+     */
+    public function questions(): Response
+    {
+        return $this->render('questions.html.twig');
+    }
 }
