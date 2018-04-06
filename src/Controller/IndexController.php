52c3e2f204432749521904109522e80a70a02066
DreDasLT <dre2kas@gmail.com>
2018-04-06 22:48:36 +0300

    Fixed routing problem.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
 	/**
-	 * @Route("/Pagrindinis", name="Pagrindinis")
+	 * @Route("/", name="index")
 	 * @return Response
 	 */
 	public function index(): Response
