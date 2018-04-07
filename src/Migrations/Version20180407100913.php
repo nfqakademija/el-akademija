c2d799bb2033cc287ff85f4cafc553afe7b77a6b
Mantas Ramančionis <mantas.ramancionis@gmail.com>
2018-04-07 13:58:55 +0300

    added migration

---
+++ src/Migrations/Version20180407100913.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180407100913 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE question ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_B6F7494EA76ED395 ON question (user_id)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EA76ED395');
+        $this->addSql('DROP INDEX IDX_B6F7494EA76ED395 ON question');
+        $this->addSql('ALTER TABLE question DROP user_id');
+    }
+}
