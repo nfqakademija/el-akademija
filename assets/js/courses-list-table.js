b0d588d3ac4d9f596d8f6556ec7276d81cc5499c
DreDasLT <dre2kas@gmail.com>
2018-04-08 21:36:29 +0300

    Added link to footer, NFQ Facebook. Added link to sidebar logo. Removed courses list, now in index page renders current course of Kaunas city.

--- assets/js/courses-list-table.js
+++ assets/js/courses-list-table.js
@@ -7,7 +7,7 @@ class Courses_List_Table extends React.Component {
     constructor() {
         super();
         this.state = {
-            courses: []
+            courses: null
         }
     }
 
@@ -23,32 +23,48 @@ class Courses_List_Table extends React.Component {
 
     render() {
 
-        const coursesList = this.state.courses.map(course => {
-            return (
-                <tr key={course['id']}>
-                    <td scope="row">{course['id']}</td>
-                    <td>{course['name']}</td>
-                    <td>{course['start']}</td>
-                    <td>{course['end']}</td>
-                </tr>
-            )
-        });
-        return (
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Start</th>
-                    <th scope="col">End</th>
-                </tr>
-                </thead>
-                <tbody>
-                {coursesList}
-                </tbody>
-            </table>
+        if(!this.state.courses) {
+            return <div></div>;
+        } else {
+            const coursesList = this.state.courses.map(course => {
+                return (
+                    <tr key={course['id']}>
+                        <td scope="row">{course['id']}</td>
+                        <td>{course['name']}</td>
+                        <td>{course['start']}</td>
+                        <td>{course['end']}</td>
+                    </tr>
+                )
+            });
 
-        )
+
+            let today = new Date();
+            const currentCourse = this.state.courses.find(course => {
+                    let startdate = new Date(course.start);
+                    let enddate = new Date(course.end);
+                    console.log(startdate + " " + enddate);
+                    return course.name === "Kaunas | Pavasario semestras" && startdate < today && enddate > today;
+                }
+            );
+            return (
+                /*<table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Start</th>
+                        <th scope="col">End</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {coursesList}
+                    </tbody>
+                </table>*/
+                <h6 class="currentCourse">{currentCourse.name}</h6>
+
+
+            )
+        }
     }
 }
 
