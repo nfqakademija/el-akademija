8c847880440c1c005053f65a7073c9d999efa1a7
Mantas Ramančionis <mantas.ramancionis@gmail.com>
2018-04-21 15:51:38 +0300

    added migration

---
+++ src/Migrations/Version20180421121001.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180421121001 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE homework_link ADD grade SMALLINT DEFAULT NULL, ADD comment LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE lecture ADD description TEXT NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE homework_link DROP grade, DROP comment');
+        $this->addSql('ALTER TABLE lecture DROP description');
+    }
+}
