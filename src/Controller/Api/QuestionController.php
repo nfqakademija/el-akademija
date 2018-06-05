5d561815b6b4e263bd3d338a16da068e32dda0ce
Mantas Ramančionis <mantas.ramancionis@gmail.com>
2018-06-05 14:40:41 +0300

    updated question controller

--- src/Controller/Api/QuestionController.php
+++ src/Controller/Api/QuestionController.php
@@ -38,6 +38,29 @@ class QuestionController extends BaseApiController
 	}
 
 	/**
+	 * @Route("/new", name="new", methods={"POST"})
+	 * @param Request $request
+	 * @return JsonResponse
+	 */
+	public function new(Request $request): JsonResponse
+	{
+		$obj = new Question();
+		$form = $this->createForm(QuestionType::class, $obj, ['csrf_protection' => false]);
+		$form->handleRequest($request);
+
+		if (!$form->isSubmitted())
+			return $this->jsonService->parametersMissing($form);
+		if (!$form->isValid())
+			return $this->jsonService->formErrors($form);
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($obj);
+		$em->flush();
+		return $this->jsonService->successData([
+			'question' => $obj->getId()
+		]);
+	}
+
+	/**
 	 * @Route("/show", name="show_all", methods={"GET"})
 	 * @param Request $request
 	 * @return JsonResponse
